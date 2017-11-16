5f22dbb261e611dc556db04ad46f31ec175893f7
Evan Candler <emcandler21@gmail.com>
2017-11-16 00:32:26 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,3 +15,12 @@ function whileLoop(n) {
       console.log (--countdown)
   return 'done';
 }
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >=
+  0.5
+  }
+  do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+}
